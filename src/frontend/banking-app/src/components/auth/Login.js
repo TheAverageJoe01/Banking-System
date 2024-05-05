eda8508dd11452819eb2827058537dbab9fe1413
Swattle1 <samwattle99@gmail.com>
2024-05-05 15:09:04 +0100

    feat(frontend): Create user

--- src/frontend/banking-app/src/components/auth/Login.js
+++ src/frontend/banking-app/src/components/auth/Login.js
@@ -1,16 +1,34 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
 
 function Login() {
   const [username, setUsername] = useState('');
+  const [name, setName] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const navigate = useNavigate();
 
   const validateForm = () => {
     if (!username || !password) {
+      setError('Username and password are required');
+      return false;
+    }
+    setError('');
+    return true;
+  };
+
+  const validateCreateForm = () => {
+    if (!name || !username || !password) {
       setError('Username and password are required');
       return false;
     }
@@ -52,30 +70,112 @@ function Login() {
     }
   };
 
+  const handleCreateUser = async (event) => {
+    event.preventDefault();
+    if (!validateCreateForm()) return;
+    setLoading(true);
+
+    const userData = {
+      name: name,
+      email: username,
+      password: password
+    };
+
+    try {
+      const response = await fetch('http://localhost:8000/users/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+      });
+
+      setLoading(false);
+
+      if (response.ok) {
+        window.location.reload();
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setLoading(false);
+      setError('An error occurred. Please try again later.');
+    }
+  };
+
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username:</label>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit" disabled={loading}>
-          {loading ? 'Logging in...' : 'Login'}
-        </button>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-      </form>
+      <Container className="d-flex vh-100 justify-content-center align-items-center">
+        <Form onSubmit={handleSubmit}>
+          <h1 className='d-flex justify-content-center'>Welcome to Big Bank</h1>
+          <Form.Group className="mb-3" controlId="formBasicUsername">
+            <Form.Label>Username</Form.Label>
+            <Form.Control type="username" placeholder="Enter username" value={username}
+              onChange={(e) => setUsername(e.target.value)} />
+            <Form.Text className="text-muted">
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)} />
+          </Form.Group>
+
+          <Container className='d-flex justify-content-between'>
+            <Button variant="success" type="submit" disabled={loading}>
+              {loading ? 'Logging in...' : 'Login'}
+            </Button>
+
+            <Button variant="info" onClick={handleShow} disabled={loading}>
+              Create User
+            </Button>
+          </Container>
+          {error && <p style={{ color: 'red' }}>{error}</p>}
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>Create User</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicName">
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control type="username" placeholder="Enter Name" value={name}
+                    onChange={(e) => setName(e.target.value)} />
+                  <Form.Text className="text-muted">
+                  </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" placeholder="Enter Email" value={username}
+                    onChange={(e) => setUsername(e.target.value)} />
+                  <Form.Text className="text-muted">
+                  </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)} />
+                </Form.Group>
+              </Form>
+              {error && <p style={{ color: 'red' }}>{error}</p>}
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button variant="primary" onClick={handleCreateUser}>
+                {loading ? 'Signing up...' : 'Signup'}
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </Form>
+
+
+      </Container>
     </div>
   );
 }
